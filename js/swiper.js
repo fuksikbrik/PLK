b830ca6b81e92fae66e22ab7faf5c5db28a8ef4c
fuksikbrik <93256876+fuksikbrik@users.noreply.github.com>
2023-07-23 17:47:34 +0300

    /

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+  // slidesPerView: 4,
+  spaceBetween: 20,
+  Infinity:true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
